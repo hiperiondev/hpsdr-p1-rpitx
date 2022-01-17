88a6d891c6f5ced1880b4d9e051a50fce18ceba4
egonzalez <egonzalez@msi.notebook.hiperion.local>
2022-01-17 18:12:34 -0300

    Add library for config file

--- build/hpsdr/subdir.mk
+++ build/hpsdr/subdir.mk
@@ -1,7 +1,3 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../hpsdr/hpsdr_debug.c \
